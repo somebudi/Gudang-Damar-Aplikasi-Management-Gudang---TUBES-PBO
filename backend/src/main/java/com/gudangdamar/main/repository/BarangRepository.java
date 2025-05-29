b387fed2cf0ac8c09a18283f2f04f226a46f81ca
Alvin Ababil <neoryumasil@gmail.com>
2025-05-29 13:26:11 +0700

    Baikin File Dikit Ngilangin DTO

--- backend/src/main/java/com/gudangdamar/main/repository/BarangRepository.java
+++ backend/src/main/java/com/gudangdamar/main/repository/BarangRepository.java
@@ -1,14 +1,7 @@
 package com.gudangdamar.main.repository;
 
 import com.gudangdamar.main.model.Barang;
-import com.gudangdamar.main.dto.BarangDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import java.util.List;
 
-public interface BarangRepository extends JpaRepository<Barang, Integer> {
 
-    //@Query("SELECT NEW com.gudangdamar.main.dto.BarangDTO(b.nama, SUM(b.harga.jumlah), AVG(b.harga.harga)) " +
-       //"FROM Barang b GROUP BY b.nama")
-    //List<BarangDTO> countBarangByNama();
-}
+public interface BarangRepository extends JpaRepository<Barang, Integer> {}
