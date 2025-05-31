12bf44337431fe411cc8ff2c96730f895112c45b
Kabims <biman7217@gmail.com>
2025-05-31 13:01:56 +0700

    search(part 1)

--- backend/src/main/java/com/gudangdamar/main/repository/BarangRepository.java
+++ backend/src/main/java/com/gudangdamar/main/repository/BarangRepository.java
@@ -1,7 +1,12 @@
 package com.gudangdamar.main.repository;
 
-import com.gudangdamar.main.model.Barang;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.gudangdamar.main.model.Barang;
 
-public interface BarangRepository extends JpaRepository<Barang, Integer> {}
+
+public interface BarangRepository extends JpaRepository<Barang, Integer> {
+    List<Barang> findByNamaContainingIgnoreCase(String nama);
+}   
